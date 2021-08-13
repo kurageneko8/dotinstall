6076eb16ef1b44353a91ea6746254e7a60f764a5
ryota_windows <ryota.ddd@gmail.com>
2021-08-13 09:48:34 +0900

    This is my first commit

---
+++ JavascriptBasics/js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+{
+    // console.log(Math.random());
+
+    // Math.floor(Math.random() * (max + 1 -min)) + min   
+
+   console.log( Math.floor(Math.random() * 6) + 1) ;
+
+}
---
+++ MyNumbersGame/js/main.js
@@ -0,0 +1,65 @@
+'use strict';
+
+{
+    class Panel {
+        constructor() {
+            this.el = document.createElement('li');
+            this.el.classList.add('pressed');
+            this.el.addEventListener('click', () => {
+                this.check();
+            });
+        }
+
+        getEl() {
+            return this.el;
+        }
+
+
+        activate(num) {
+            this.el.classList.remove('pressed');
+            this.el.textContent = num;
+        } 
+
+        check() {
+            if (currentNum === parseInt(this.el.textContent, 10)) {
+                this.el.classList.add('pressed');
+                currentNum++;
+            }
+        }
+    }
+    
+    class Board {
+        constructor() {
+            this.panels = [];
+            for(let i = 0; i < 4; i++) {
+                this.panels.push(new Panel());
+            }
+            this.setup();
+        }
+
+        setup() {
+            const board = document.getElementById('board');
+            this.panels.forEach(panel => {
+                board.appendChild(panel.getEl());
+            });
+        }
+
+        activate() {
+            const nums = [0,1,2,3];
+
+            this.panels.forEach(panel => {
+                const num = nums.splice(Math.floor(Math.random() * nums.length), 1)[0];
+                panel.activate(num);
+            });
+        }
+    }
+
+    const board = new Board();
+
+    let currentNum = 0;
+
+    const btn = document.getElementById('btn');
+    btn.addEventListener('click', () => {
+        board.activate();
+    });
+}
